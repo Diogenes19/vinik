8e71fe2934d9becb5b0987cf9dc41953acc5a82a
Diogenes19 <164023034+Diogenes19@users.noreply.github.com>
2025-02-14 15:31:15 -0300

    gyt

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 document.addEventListener('DOMContentLoaded',function()){
     const aumentaFonteBotao=document.getElementByuld('aumentar-fonte');
     const diminuiFonteBotao=document.getElementByuld('diminui-fonte');
-}
+    let tamanhoAtualFont=1
+    aumentaFonteBotao.addEventListener('click',function(){
+        tamanhoAtualFont+=0.1;
+        document.body.requestPointerLock.fontSize=${tamanhoAtualFont}rem``
+    })

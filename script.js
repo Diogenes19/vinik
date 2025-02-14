a5faca294e1858327e1b8001d0edbc19b06868dc
Diogenes19 <164023034+Diogenes19@users.noreply.github.com>
2025-02-14 15:24:32 -0300

    iuhiuhyi

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded',function()){
+    const aumentaFonteBotao=document.getElementByuld('aumentar-fonte');
+    const diminuiFonteBotao=document.getElementByuld('diminui-fonte');
+}
